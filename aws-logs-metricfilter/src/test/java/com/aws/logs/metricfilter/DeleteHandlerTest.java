03766389a206385ff76d0d9f323f60328fc4a8bd
Ryan Lohan <ryanloha@amazon.com>
2019-05-24 10:18:44 -0700

    Updated baseline with latest RPDK setup

---
+++ aws-logs-metricfilter/src/test/java/com/aws/logs/metricfilter/DeleteHandlerTest.java
@@ -0,0 +1,52 @@
+package com.aws.logs.metricfilter;
+
+import com.aws.cfn.proxy.AmazonWebServicesClientProxy;
+import com.aws.cfn.proxy.AmazonWebServicesRequestFunctionV2;
+import com.aws.cfn.proxy.Logger;
+import com.aws.cfn.proxy.OperationStatus;
+import com.aws.cfn.proxy.ProgressEvent;
+import com.aws.cfn.proxy.ResourceHandlerRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+public class DeleteHandlerTest {
+
+    @Mock
+    private AmazonWebServicesClientProxy proxy;
+
+    @Mock
+    private Logger logger;
+
+    @Before
+    public void setup() {
+        proxy = mock(AmazonWebServicesClientProxy.class);
+        logger = mock(Logger.class);
+    }
+
+    @Test
+    public void test_HandleRequest_SimpleSuccess() {
+        final DeleteHandler handler = new DeleteHandler();
+
+        final ResourceModel model = ResourceModel.builder().build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(model)
+            .build();
+
+        final ProgressEvent response = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response, is(not(nullValue())));
+        assertThat(response.getStatus(), is(equalTo(OperationStatus.SUCCESS)));
+        assertThat(response.getResourceModel(), is(equalTo(request.getDesiredResourceState())));
+    }
+}
