8fc91103b3c9b9fd8032a8bc88beb69522755a3b
Adam Daily <adaily@amazon.com>
2019-09-24 16:33:40 -0700

    Remove retry policy overrides in client builders
    The retry policy we currently have implemented is a little too heavy
    handed, so we'll stick with the client default. I've kept the
    ClientBuilder class however in case we do want to override in the
    future.

--- aws-logs-destination/src/main/java/com/amazonaws/logs/destination/ClientBuilder.java
+++ aws-logs-destination/src/main/java/com/amazonaws/logs/destination/ClientBuilder.java
@@ -1,15 +1,10 @@
 package com.amazonaws.logs.destination;
 
-import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
-import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
 
 public class ClientBuilder {
     public static CloudWatchLogsClient getClient() {
         return CloudWatchLogsClient.builder()
-            .overrideConfiguration(ClientOverrideConfiguration.builder()
-                .retryPolicy(RetryPolicy.builder().numRetries(16).build())
-                .build())
             .build();
     }
 }
