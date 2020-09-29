a95a7ce8f9aafc29cb5935a883e7aa0e07439615
Wenbing Li <libruce@amazon.com>
2020-09-28 18:14:52 -0700

    Use higher retry number to improve resource resilience

--- aws-logs-loggroup/src/main/java/software/amazon/logs/loggroup/ClientBuilder.java
+++ aws-logs-loggroup/src/main/java/software/amazon/logs/loggroup/ClientBuilder.java
@@ -1,14 +1,24 @@
 package software.amazon.logs.loggroup;
 
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryPolicy;
+import software.amazon.awssdk.core.retry.conditions.RetryCondition;
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
 import software.amazon.cloudformation.LambdaWrapper;
 
 public class ClientBuilder {
     private ClientBuilder() {}
 
+    private static final RetryPolicy RETRY_POLICY =
+        RetryPolicy.builder()
+            .numRetries(6)
+            .retryCondition(RetryCondition.defaultRetryCondition())
+            .build();
+
     public static CloudWatchLogsClient getClient() {
         return CloudWatchLogsClient.builder()
-                .httpClient(LambdaWrapper.HTTP_CLIENT)
-                .build();
+            .httpClient(LambdaWrapper.HTTP_CLIENT)
+            .overrideConfiguration(ClientOverrideConfiguration.builder().retryPolicy(RETRY_POLICY).build())
+            .build();
     }
 }
