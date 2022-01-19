ee10e5e347490746d48a20712437eb2ef753e0cf
Huzaifa <42986799+waifa@users.noreply.github.com>
2022-01-18 17:38:53 -0800

    update TranslatorTest.java to fix missing import bug

--- aws-logs-metricfilter/src/test/java/software/amazon/logs/metricfilter/TranslatorTest.java
+++ aws-logs-metricfilter/src/test/java/software/amazon/logs/metricfilter/TranslatorTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
